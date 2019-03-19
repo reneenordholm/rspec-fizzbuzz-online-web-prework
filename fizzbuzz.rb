25413831ee794571066147708ab4818007f98b43
Renee Nordholm <reneenordholm@gmail.com>
2019-03-19 11:54:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(fizz_3)
-  "#{fizz_3}/3"
+def fizzbuzz(3)
+  "#{3}/3"
     return "Fizz"
 end
 
-def fizzbuzz(fizz_5)
-  "#{value}/5"
+def fizzbuzz(5)
+  "#{5}/5"
     return "Buzz"
 end
