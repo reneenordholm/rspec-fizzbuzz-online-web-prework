04f12dd95230383cebea9c184efda5c5f4c1cc5e
Renee Nordholm <reneenordholm@gmail.com>
2019-03-19 11:35:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,20 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(fizz_3)
-  if fizz_3/3
+  fizz_3/3
     return "Fizz"
- end
-
-def fizzbuzz(fizz_5)
-  if fizz_5/5
-    return "Buzz"
-end
-
-  elsif value/3 && value/5
-    return "FizzBuzz"
-
-  else
-    return "nil"
-
- end
  end
