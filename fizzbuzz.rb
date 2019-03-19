1079e56a2386d8559580853712f17f7a263c103e
Renee Nordholm <reneenordholm@gmail.com>
2019-03-19 11:55:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(3)
-  "#{3}/3"
-    return "Fizz"
-end
-
-def fizzbuzz(5)
-  "#{5}/5"
-    return "Buzz"
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  end
 end
