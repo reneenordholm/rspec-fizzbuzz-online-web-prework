641a34224e6b32bee5a7333a28147b5e30b83a54
Renee Nordholm <reneenordholm@gmail.com>
2019-03-19 11:50:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(value)
-  if value == /3
+  if value/3
     return "Fizz"
-  elsif value == /5
+  elsif value/5
     return "Buzz"
   else
     return ""
