2e793514fdb8ec11ddd2aa97a3b1adf064fc5fac
Renee Nordholm <reneenordholm@gmail.com>
2019-03-19 12:11:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz"
-  if int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
-  if int % 3 == 0 && int % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz" # Go fiz
-  end
+  else
+    ""
 end
 end
